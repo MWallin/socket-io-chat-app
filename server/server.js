b2174bd18f23b00c680f7dee39ed95d681402314
Mikael Wallin <mikael.wallin@gmail.com>
2017-03-19 14:58:04 +0100

    fix typo

--- server/server.js
+++ server/server.js
@@ -137,7 +137,7 @@ io.on( "connection", ( socket ) => {
 
       io.to( user.room ).emit( "updateUserList", users.getUserList( user.room ) )
 
-      io.to( user.room ).emit( "newMessage", generateMessage( "Admin", `${user.name} has left the chat!` ))
+      io.to( user.room ).emit( "newMessage", generateMessage( "Admin", `${user.name} has left the chat!` ) )
 
     }
 
